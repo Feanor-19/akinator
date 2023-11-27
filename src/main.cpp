77f63126d9221cd5a1e492ed4211803b87c10b29
Filipp <sap.phil.v@mail.ru>
2023-11-27 22:13:18 +0300

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,9 @@
 #include "akinator.h"
 #include "mystring.h"
 #include "../../../mylibheaders/tree.h"
-// TODO - поправить инклюды, чтобы их воспринимал компилятор (VS CODE все понимает уже)
 
 int main()
 {
-    // Tree tree = read_tree_from_file("database_silm.txt");
-    // TREE_DUMP(&tree, 0);
-
     Database database = init_and_load_database();
 
     main_loop(&database);
